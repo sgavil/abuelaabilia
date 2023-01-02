e4e68d81100fbdc55bc3201a1fb1c392f5517aef
sgavil <ssjsrgx@gmail.com>
2023-01-02 18:24:49 +0100

    Start

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+/*!
+* Start Bootstrap - Bare v5.0.7 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+function moveToMain(){
+    window.location.href="main.html"
+}
